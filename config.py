ded8f51b8312751954e374c400cc6ed39f7cfc76
hashtanveer <123923230+hashtanveer@users.noreply.github.com>
2023-08-31 16:26:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "52919388xxx:AAHqCQM0XxxxxxxxxxonG6oqVSFsg"
+MONGODB_CLIENT = "mongodb+srv://user:password@cluster0.4xxxo.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
+HOST = "https://example.com"#Hosting domain
